9c97ba15627aac8eedc9f0660a271b2ecebbf414
JALG27 <41845529+JALG27@users.noreply.github.com>
2023-04-03 20:37:23 -0400

    Update post.controller.ts

--- backend/src/controllers/post.controller.ts
+++ backend/src/controllers/post.controller.ts
@@ -1,9 +1,7 @@
 import { Request, Response} from 'express'
-import Post, { IPost } from '../models/post'
+import Post from '../models/post'
 
 export const newPost = async (req: Request, res: Response): Promise<Response> => {
-
-
     const Newpost = new Post (req.body);
     await Newpost.save();
     return res.status(201).json({Newpost, msg:'Post registered succesfully'})
